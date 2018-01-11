8acc13555e0a3c5e0cd73de23bd27738a5687a82
Jacob Horvat <horvatjake@gmail.com>
2018-01-10 17:36:29 -0800

    added some code to index, learned about JS and the DOM, and worked on business positioning.

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <?php $pageTitle = "Home | Team Blyder"; ?>
 
 <?php include("inc/header.php"); ?>
@@ -8,5 +7,10 @@
 
 <!-- End Content -->
 </div>
+
+<h2>Welcome to Team Blyder!</h2>
+<h3>Find great experiences for your team!</h3>
+
+
 
 <?php include("inc/footer.php"); ?>
